961cf264c9ac3cfa9bcf26d5ae25b3bd1fddf417
aldoGC9092 <aldo.garcia9092@alumnos.udg.mx>
2024-09-29 19:08:55 -0600

    Ready to deploy

--- src/main.js
+++ src/main.js
@@ -3,4 +3,47 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCriwju2YyRPl0FTTs8_7b-KWYR6nJWMgw",
+  authDomain: "fir-7a22a.firebaseapp.com",
+  projectId: "fir-7a22a",
+  storageBucket: "fir-7a22a.appspot.com",
+  messagingSenderId: "916661834739",
+  appId: "1:916661834739:web:d4d4171237350ac0a97886"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log(user)
+      const usuarioActivo ={
+        email: user.email,
+        uid: user.uid
+      }
+     store.dispatch('detectarUsuario', usuarioActivo)
+     console.log(usuarioActivo)
+    // ...
+  } else {
+    console.log(user)
+    store.dispatch('detectarUsuario', user)
+    // User is signed out
+    // ...
+  } 
+});
+
 createApp(App).use(store).use(router).mount('#app')
